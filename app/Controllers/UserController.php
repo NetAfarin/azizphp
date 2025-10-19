284f7432bbd5859fa7d5759c4bfdd84fdb83f2fb
Setayesh <3tayesh.mp@gmail.com>
2025-10-19 21:34:56 +0330

    add user dashboard layouts in user dashboard complete just some texts need to change

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -285,7 +285,7 @@ class UserController extends Controller
             'errors' => $errors,
         ]);
     }
- public function add()
+    public function add()
     {
         $service = Service::query()->where('parent_id', '<>', 0)->where("deleted", "=", 0)->get();
         $userRole = UserType::all();
@@ -300,6 +300,14 @@ class UserController extends Controller
             'userRole' => $userRole,
         ]);
     }
+    public function dashboard()
+    {
+        $this->view('user/originalView/userDashboard', [
+            'title' => __('dashboard'),
+            'first_name' => !empty($_SESSION['user_name']) ? $_SESSION['user_name'] : "",
+            'last_name' => !empty($_SESSION['last_name']) ? $_SESSION['last_name'] : "",
+        ]);
+    }
     public function manageUsers()
     {
         $service = Service::query()->where('parent_id', '<>', 0)->where("deleted", "=", 0)->get();
@@ -310,7 +318,7 @@ class UserController extends Controller
             'title' => __('manage_users'),
         ]);
     }
-public function otpPage()
+    public function otpPage()
     {
         $errors = [];
         $success = false;
---
+++ app/Views/user/originalView/userDashboard.php
@@ -0,0 +1,115 @@
+<div class="d-lg-none">
+    <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebar"
+            aria-controls="sidebar">
+        <i class="fa fa-bars"></i>
+    </button>
+</div>
+<?php include BASE_PATH . '/app/Views/components/layout.php'; ?>
+
+<div class="row  mt-5">
+    <div class="col-8">
+        <div class="row">
+        <div class="col">
+            <div class="card  shadow-md " >
+                <div class="card-header text-center fw-bold bg-transparent">وضعیت رزروها</div>
+                <div class="card-body">
+                    <p class="card-text">کوتاهی مو با سارا</p>
+                    <p class="card-text">ترمیم ناخن با هرکی</p>
+                </div>
+                <div class="custom-card-footer"><span></span></div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card  shadow-md" >
+                <div class="card-header text-center fw-bold bg-transparent">خدمات محبوب شما</div>
+                <div class="card-body">
+                    <p class="card-text">کوتاهی مو با سارا</p>
+                    <p class="card-text">ترمیم ناخن با هرکی</p>
+                </div>
+                <div class="custom-card-footer"><span></span></div>
+            </div>
+        </div>
+        </div>
+        <div class="mt-5">
+        <div class="d-flex rounded bg-white flex-column justify-content-evenly shadow-md">
+            <div class="p-2  text-start fw-bold">نظر شما مهم است!</div>
+            <div class="p-2 text-secondary text-center ">رنگ موی شما ۲ روز پیش انجام شد. لطفاً تجربه‌تان را به اشتراک بگذارید.</div>
+            <div class="p-2 text-secondary text-center">
+                <button class="btn btn-primary">ثبت نظر</button>
+            </div>
+        </div>
+        </div>
+        <div class="mt-5 d-flex rounded bg-white flex-column justify-content-evenly shadow-md">
+            <div class="p-2  text-start fw-bold">نظر شما مهم است!</div>
+            <div class="p-2 text-secondary text-center ">رنگ موی شما ۲ روز پیش انجام شد. لطفاً تجربه‌تان را به اشتراک بگذارید.</div>
+            <div class="p-2 text-secondary text-center">
+                <button class="btn btn-primary">ثبت نظر</button>
+            </div>
+        </div>
+        <div class="mt-5 d-flex rounded bg-white flex-column justify-content-evenly shadow-md">
+            <div class="p-2  text-start fw-bold">نظر شما مهم است!</div>
+            <div class="p-2 text-secondary text-center ">رنگ موی شما ۲ روز پیش انجام شد. لطفاً تجربه‌تان را به اشتراک بگذارید.</div>
+            <div class="p-2 text-secondary text-center">
+                <button class="btn btn-primary">ثبت نظر</button>
+            </div>
+        </div>
+        <div class="mt-5">
+            <div class="d-flex rounded bg-white p-4 flex-column justify-content-evenly shadow-md"
+                 style=" border-radius: 8px">
+                <div class="p-2 text-start fw-bold">خدمات محبوب مجموعه</div>
+                <div class="d-flex justify-content-center gap-3">
+                    <div class="d-flex flex-column justify-content-center"
+                         style="border: 1px solid #DEDEDE; background: #F5F5F5; border-radius: 8px; width: 267px; height: 99px;">
+                        <h6>رنگ مو</h6>
+                        <p class="text-secondary">مدت زمان: 2 ساعت</p>
+                    </div>
+
+                    <div class="d-flex flex-column justify-content-center"
+                         style="border: 1px solid #DEDEDE; background: #F5F5F5; border-radius: 8px; width: 267px; height: 99px;">
+                        <h6>رنگ مو</h6>
+                        <p class="text-secondary">مدت زمان: 2 ساعت</p>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
+
+    </div>
+
+    <div class="col-4">
+        <div class="card  shadow-md mb-3" >
+            <div class="card-header text-center fw-bold bg-transparent">وضعیت رزروها</div>
+            <div class="card-body">
+                <p class="card-text">کوتاهی مو با سارا</p>
+                <p class="card-text">ترمیم ناخن با هرکی</p>
+            </div>
+            <div class="custom-card-footer"><span></span></div>
+        </div>
+
+        <div class="mt-5">
+            <div class="shadow-md bg-white rounded">
+                <h2 class="text-start">نکات مهم</h2>
+                <div class=" d-flex flex-column justify-content-start align-items-center" style="direction: ltr"
+                >
+                        <span class="tips">.موها را بعد از کراتین مرطوب نگه دارید<i class="fa fa-circle text-primary px-2"></i></span>
+                        <span class="tips">.موها را بعد از کراتین مرطوب نگه دارید<i class="fa fa-circle text-primary px-2"></i></span>
+                        <span class="tips">.موها را بعد از کراتین مرطوب نگه دارید<i class="fa fa-circle text-primary px-2"></i></span>
+                        <span class="tips">.موها را بعد از کراتین مرطوب نگه دارید<i class="fa fa-circle text-primary px-2"></i></span>
+                        <span class="tips">.موها را بعد از کراتین مرطوب نگه دارید<i class="fa fa-circle text-primary px-2"></i></span>
+                        <span class="tips">.موها را بعد از کراتین مرطوب نگه دارید<i class="fa fa-circle text-primary px-2"></i></span>
+                </div>
+            </div>
+
+
+
+
+        </div>
+
+    </div>
+
+
+</div>
+</div>
+</div>
